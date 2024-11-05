a8553989be493eb71c308863c278dad362698e50
dimmaneni009 <dimmaneni009@gmail.com>
2024-11-05 10:22:23 -0600

    add this

---
+++ Add.java
@@ -0,0 +1,13 @@
+public class ArraysEx {
+	public static void main(String[] args) {
+
+		int i[] = { 23, 24, 25, 26, 27 };
+
+		int[] j = new int[5];
+		j[0] = 1;
+		j[1] = 2;
+		j[4] = 3;
+
+		j[0] = 6; //RE-Assigning value
+		
+		
