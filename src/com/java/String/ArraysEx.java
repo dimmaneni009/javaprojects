5dfc1411525635cc719c8e63e8bfe58d23ca7b65
dimmaneni009 <dimmaneni009@gmail.com>
2024-11-05 20:03:46 -0600

    add

--- src/com/java/String/ArraysEx.java
+++ src/com/java/String/ArraysEx.java
@@ -2,31 +2,33 @@ package com.java.String;
 
 public class ArraysEx {
 	public static void main(String[] args) {
-		
-		int i[]= {23,24,25,26,27};
-		
-		int[] j=new int[5];
-		j[0]=1;
-		j[1]=2;
-		j[4]=3;
-		j[0]=6;  //RE-Assigning 
-		//System.out.println(j[0]);
-		
-	
-		String k[]=new String[4];
-		k[1]="hello";
-		
+
+		int i[] = { 23, 24, 25, 26, 27 };
+
+		int[] j = new int[5];
+		j[0] = 1;
+		j[1] = 2;
+		j[4] = 3;
+
+		j[0] = 6; //RE-Assign
+
+		// System.out.println(j[0]);
+
+		String k[] = new String[4];
+		k[1] = "hello";
+
 		System.out.println(k[1]);
+
+		boolean m[] = new boolean[6];
+		m[0]= false;
+		m[1]= true;
 		
-		
-		boolean m[]=new boolean[6];
-		
-		
-		byte n[]=new byte[3];
-		
-		float f[]=new float[2];
-		
+		byte n[] = new byte[3];
+		n[0]=25;
+
+		float f[] = new float[2];
+		f[0]=10.0f;
+
 	}
-	
 
 }
---
+++ src/com/java/String/Num.java
@@ -0,0 +1,20 @@
+package com.java.String;
+
+
+public class Num {
+	public static void main(String[] args){
+		
+	}
+	
+	int a =1; 
+	int b = 2;
+	
+	int c = a + b;
+	
+	int add(){
+		return c;
+	}
+	
+	
+	
+}
